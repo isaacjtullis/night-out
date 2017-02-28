607c013145bcac8295d5e53525f71af37b94d0bb
Isaac Tullis <isaacjtullis@gmail.com>
2017-02-28 11:03:05 -0700

    Added styling to zipcode form field

--- react/src/NewLocationForm.js
+++ react/src/NewLocationForm.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const NewLocationForm = props => {
   return (
-    <form onSubmit={props.handleZipCode}>
-      <button><i className="fa fa-sticky-note-o" aria-hidden="true">New ZipCode</i></button>
-      <input
-        type="text"
-        placeholder="search"
-        onChange={props.handleNewZipCode}
-        >
-      </input>
-    </form>
+    <div className="zipcode-form">
+      <form onSubmit={props.handleZipCode}>
+        <button>Add ZipCode</button>
+        <input
+          type="text"
+          placeholder="Zipcoddeeee"
+          onChange={props.handleNewZipCode}
+          >
+        </input>
+      </form>
+    </div>
   )
 };
 
