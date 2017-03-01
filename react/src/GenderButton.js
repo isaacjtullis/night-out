3643b96955be149a93003c01850c2d540bb9d469
Isaac Tullis <isaacjtullis@gmail.com>
2017-02-28 17:26:52 -0700

    added styling to movie description

--- react/src/GenderButton.js
+++ react/src/GenderButton.js
@@ -4,18 +4,25 @@ const GenderButton = (props) => {
   let weather = props.weather.map(currentTemp=>{
     return(currentTemp.temp)
   })
+  let description = ""
   let whatIShouldWear = ""
   if (props.genderNotice === "male" && weather < 30){
+    description = "It is cold! Best tips for guys to look chill when its chilly. Alright that pun killed us to #corny"
     whatIShouldWear = "http://www.gq.com/gallery/what-to-wear-today-december-2016"
   } else if (props.genderNotice === 'male' && weather < 60) {
+    description = "Some springs clothing ideas for the guy who don't know shit"
     whatIShouldWear = "http://www.esquire.com/style/advice/g751/mens-spring-clothing-advice-022411/"
   } else if (props.genderNotice === 'male' && weather < 90) {
+    description = "This site is for the guy who wears flip flops with socks. You can do better"
     whatIShouldWear = "http://www.primermagazine.com/2015/spend/summer-style-essentials"
   } else if (props.genderNotice === 'female' && weather < 30) {
+    description = "Stay warm and stylish. It is possible."
     whatIShouldWear = "http://www.instyle.com/how-tos/what-to-wear-to-work-winter"
   } else if (props.genderNotice === 'female' && weather < 60) {
+    description = "Insert deep quote about being stylish but seriously, these are some great outfits for the weather right now"
     whatIShouldWear = "http://stylecaster.com/casual-spring-outfits/"
   } else if (props.genderNotice === 'female' && weather < 90) {
+    description = "Warm and cute. Check these styles out :)"
     whatIShouldWear = "http://www.harpersbazaar.com/summer-fashion/"
   }
   return(
@@ -30,7 +37,9 @@ const GenderButton = (props) => {
           <button type="button" onClick={props.handleFemaleclick}><i className="fa fa-female fa-3x" aria-hidden="true"></i></button>
         </div>
       </div>
-      <div>{whatIShouldWear}</div>
+      <div className="clothing-ideas">
+        <a href={whatIShouldWear}>{description}</a>
+      </div>
     </div>
   )
 }
--- react/src/Movie.js
+++ react/src/Movie.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 const Movie = (props) => {
-  let movieDetails;
+  let movieDetails = '';
   let showMovieDetails = props => {
     if(props.selected){
       movieDetails =
         <div className="col-md-8" onClick={props.handleClick}>
-          <h2>{props.overview}</h2>
+          <h4>{props.overview}</h4>
           <p>Rating:</p>
         </div>;
     }
     return movieDetails;
   }
-
   return(
     <div className="row">
-      <div className="col-md-12" id={props.id} onClick={props.handleClick}><h1>{props.title}</h1></div>
+      <div className="test" id={props.id} onClick={props.handleClick}><h1>{props.title}</h1></div>
         <div className="movie-description">
           {showMovieDetails(props)}
         </div>
